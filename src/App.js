aad55d4cbd3d7cece8d1702321ac5cb6900be306
Saadani Mohmaed Amine <saadanimohamedamine@gmail.com>
2020-10-19 22:32:39 +0200

    moviesList componet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MoviesList from './MoviesList';
 
 function App() {
+  
+  const moviesList=[
+    {
+      name:"Ip Man",
+      image:"https://pic.egybest.net/i/WmFwZndlY21ZcHBwRW1wZWNtRXZ3UG1qUHdUWWc.jpg" ,
+      rating:5,
+      year:2019
+    },
+    {
+      name:'Iron Man ',
+      image:'https://pic.egybest.net/i/WmFwZndlY212bW1tcGJtRUVjbXZtRW1USXhtam1wSQ.jpg',
+      rating:3,
+      year:2019
+    },
+    {
+      name:"Sighless",
+      image:'https://pic.egybest.net/i/WmFwZndlY21ZTE5tbW1tam1hY21ieGNOam1UYm1vdw.jpg',
+      rating:4,
+      year:2020,
+    }
+  ];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <MoviesList movies={movies.filter(movie=>
+          movie.name
+          .toLocaleLowerCase()
+          .includes(search.trim().toLocaleLowerCase()) && movie.rating>=rating
+          ) }/>
       </header>
     </div>
   );
